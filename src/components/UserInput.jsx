0185e2fe6f1ee43239f4ce3c8ac06c76131ec677
syed <syedmustaqahmed5@gmail.com>
2025-07-31 22:34:06 +0530

    investment-calculator

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,64 @@
+export default function UserInput({ onChange, userInput }) {
+  const handleInputChange = (field, value) => {
+    // Allow empty string and negative sign
+    if (value === "" || value === "-") {
+      onChange(field, value);
+    } else {
+      const parsedValue = parseFloat(value);
+      onChange(field, isNaN(parsedValue) ? "" : parsedValue);
+    }
+  };
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleInputChange("initialInvestment", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleInputChange("annualInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleInputChange("expectedReturn", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) =>
+              handleInputChange("duration", event.target.value)
+            }
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
